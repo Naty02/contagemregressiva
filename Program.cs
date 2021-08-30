3cb98857b81af09ad4ccdab922630420410730bd
Natalie Gonçalves <nataliegoncalvesdasilva02@gmail.com>
2021-08-30 00:18:01 -0300

    resolução do exercício contagemregressiva

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace contagemregressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Apontar");
+            Console.ReadLine();
+            Console.WriteLine("Preparar");
+            Console.ReadLine();
+            Console.WriteLine("Fogo");
+        }
+    }
+}
